043bce25c9672e9f6f0deac32692c446c8488c5a
vitorkolle <vitorpaeskolle@gmail.com>
2023-09-06 16:07:11 -0300

    json finalizado

--- app.js
+++ app.js
@@ -1,1 +1,61 @@
 'use strict'
+
+const info = [
+{
+    imagem: "jerome bueno.jpg",
+    nome:"Jerome Bueno",
+    mensagem:"Posso realizar o pagamento por...",
+    contato: "João Paulo",
+    tempo: "Há 30 minutos",
+    quantidade: "1"
+
+},
+{
+    imagem: "daniel silva.jpg",
+    nome:"Daniel Slva",
+    mensagem:"Márcia diz: Bom dia, como posso ajuda...",
+    contato: "Márcia",
+    tempo: "Há 30 minutos",
+    quantidade: "1"
+},
+{
+    imagem: "bruno adorno.jpg",
+    nome:"Bruno Adorno",
+    mensagem:"Bom dia, gostaria de saber quant...",
+    contato: "Joysi",
+    tempo: "Há 30 minutos",
+    quantidade: "1"
+},
+{
+    imagem: "viviane ribeiro.jpg",
+    nome:"Viviane Ribeiro",
+    mensagem:"Adorei o atendimento, foi nota 10...",
+    contato: "Geovana",
+    tempo: "Há 30 minutos",
+    quantidade: "1"
+},
+{
+    imagem: "maria joana.jpg",
+    nome:"Maria Joana",
+    mensagem:"Jonathan diz: A senhora precisa de mais a...",
+    contato: "Jonathan",
+    tempo: "Há 30 minutos",
+    quantidade: "1"
+},
+{
+    imagem: "juliana pereira.jpg",
+    nome:"Juliana Pereira",
+    mensagem:"Saulo diz: Enviarei o comprovante...",
+    contato: "Saulo",
+    tempo: "Há 30 minutos",
+    quantidade: "1"
+},
+{
+    imagem: "henrique martins.jpg",
+    nome:"Henrique Martins",
+    mensagem:"Joysi diz: Estarei enviando o comprovant...",
+    contato: "Joisy",
+    tempo: "Há 30 minutos",
+    quantidade: "1"
+}
+]
